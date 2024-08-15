cf59ee1c5b4de5cd0e915da7bdb136960694e6dc
Marco Antônio Dal Vesco <440468@unoesc.lan>
2024-08-14 22:03:56 -0300

    Add C#

---
+++ Atividades/Primeira Aula/Program.cs
@@ -0,0 +1,19 @@
+﻿// Comentário de uma linha
+
+/*
+    Comentário de
+    múltiplas linhas
+*/
+
+
+// Declarando variável
+int number;
+
+// Atribuindo valor
+number = 10;
+
+// Declarando constante
+const int DAYS_IN_WEEK = 7;
+
+// Imprimindo string concatenada
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias.");
