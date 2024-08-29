e6dda180320b1aac23bc3cdf60d1c5f06dab9ff4
Marco Antônio Dal Vesco <marco.dalvesco@gmail.com>
2024-08-29 10:40:14 -0300

    Exercício resolvido

---
+++ Atividades/Aula03-2-Atividade/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO.Compression;
+
+int[] numerosPares = new int[100];
+int[] numerosImpares = new int[100];
+
+for (int i = 0; i < numerosPares.Length * 2; i++)
+{
+    if (i % 2 == 0 && i != 0)
+    {
+        numerosPares[i / 2] = i;
+    }
+    else numerosImpares[i / 2] = i;
+}
+
+Console.WriteLine("Números Pares:");
+
+foreach (int number in numerosPares)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("");
+Console.WriteLine("Números Ímpares:");
+
+foreach (int number in numerosImpares)
+{
+    Console.WriteLine(number);
+}
