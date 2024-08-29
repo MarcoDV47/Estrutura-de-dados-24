9f9388da87b15291c32983e623daba75d164c388
Marco Antônio Dal Vesco <marco.dalvesco@gmail.com>
2024-08-28 21:27:14 -0300

    Aula 3

---
+++ Atividades/Aula02/Program.cs
@@ -0,0 +1,24 @@
+using Aula02;
+
+Person person = new Person();
+Console.WriteLine(person.Name);
+
+Person person1 
+    = new Person("Mauricio Gonzatto", 40);
+Console.WriteLine(person1.Name);
+
+Person person2 = new Person(); 
+person2.Name = "Vladmir Ilich Ulyanov";
+person2.Age = 70;
+Console.WriteLine(person2.Name);
+
+Person person3 = new Person()
+{
+    Name = "Leonel Brizola",
+    Age = 80
+};
+
+Console.WriteLine(person3.Name);
+
+Person person4 = new("Ayrton", 23);
+Console.WriteLine(person4.Name);
