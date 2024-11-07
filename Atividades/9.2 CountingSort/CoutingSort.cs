70d9a9b32c056bf3c330f64fc545d2b0e2a618a2
Marco Antônio Dal Vesco <marco.dalvesco@gmail.com>
2024-11-06 21:13:53 -0300

    ArvoreBinaria

--- Atividades/9.2 CountingSort/CoutingSort.cs
+++ Atividades/9.2 CountingSort/CoutingSort.cs
@@ -3,26 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CountingSort {
-    public class CountingSort {
-        public static List<int> CountSort(List<int> inputArray) {
-        int n = inputArray.Count;
-        int m = 0;
+namespace CountingSort
+{
+    public class CountingSort
+    {
+        public static List<int> CountSort(List<int> inputArray)
+        {
+            int n = inputArray.Count;
+            int m = 0;
 
-        for (int i = 0; i < n; i++) m = Math.Max(m, inputArray[i]);
-        List<int> countArray = new List<int>(new int[m + 1]);
+            for (int i = 0; i < n; i++) m = Math.Max(m, inputArray[i]);
+            List<int> countArray = new List<int>(new int[m + 1]);
 
-        for (int i = 0; i < n; i++) countArray[inputArray[i]]++;
+            for (int i = 0; i < n; i++) countArray[inputArray[i]]++;
 
-        for (int i = 1; i <= m; i++) countArray[i] += countArray[i - 1];
-        List<int> outputArray = new List<int>(new int[n]);
+            for (int i = 1; i <= m; i++) countArray[i] += countArray[i - 1];
+            List<int> outputArray = new List<int>(new int[n]);
 
-        for (int i = n - 1; i >= 0; i--) {
-            outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
-            countArray[inputArray[i]]--;
-        }
+            foreach (var item in countArray)
+            {
+                Console.WriteLine(item);
 
-        return outputArray;
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                outputArray[countArray[inputArray[i]] - 1] = inputArray[i];
+                countArray[inputArray[i]]--;
+            }
+
+            return outputArray;
         }
     }
 }
